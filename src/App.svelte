b6135edbbc88ff5c3dca535aa555749aa148d9d5
yhylord <yhylord@gmail.com>
2019-05-06 22:43:56 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	let result = 0, input = 0;
+
+	function add() {
+		result += input;
+	}
+
+	function subtract() {
+		result -= input;
+	}
+
+	function multiply() {
+		result *= input;
+	}
+
+	function divide() {
+		result /= input;
+	}
+</script>
+
+<input type=number bind:value={result}>
+<input type=number bind:value={input}>
+
+<button on:click={add}>+</button>
+<button on:click={subtract}>-</button>
+<button on:click={multiply}>*</button>
+<button on:click={divide}>/</button>
